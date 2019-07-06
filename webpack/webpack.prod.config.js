e29eb7ad3c2db03d9de14ab5de3e7c24c59cb3e3
hongxuWei <hongxu.wei@outlook.com>
2019-07-06 17:44:36 +0800

    [modify] extract-text-webpack-plugin -> mini-css-extract-plugin

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,40 +1,38 @@
 const config = require('./webpack.base.config')
 const merge = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const _config = {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin(),
-    new ExtractTextPlugin('css/[name].[chunkhash:8].css')
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[chunkhash:8].css'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            {
-              loader: "css-loader",
-              options: {
-                modules: {
-                  localIdentName: '[name]_[local]_[hash:base64:5]'
-                },
-                localsConvention: 'camelCaseOnly'
-              }
-            },
-            "postcss-loader",
-            {
-              loader: "less-loader",
-              options: {
-                modules: true,
-              }
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: '[name]_[local]_[hash:base64:5]'
+              },
+              localsConvention: 'camelCaseOnly'
             }
-          ],
-          publicPath: '../'
-        })
+          },
+          "postcss-loader",
+          {
+            loader: "less-loader",
+            options: {
+              modules: true,
+            }
+          }
+        ]
       }
     ]
   }
