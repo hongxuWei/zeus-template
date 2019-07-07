1306c427a9f797f68d44e7d578df537d4c4d856b
hongxuWei <hongxu.wei@outlook.com>
2019-07-07 14:40:55 +0800

    [add] optimize-css-assets-webpack-plugin

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -2,6 +2,8 @@ const config = require('./webpack.base.config')
 const merge = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const optimizeCss =  require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const _config = {
   mode: 'production',
   plugins: [
@@ -34,6 +36,16 @@ const _config = {
           }
         ]
       }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new optimizeCss()
     ]
   }
 }
